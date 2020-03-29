863212ee554c857fa0da9dde0735adee06090796
duchunxiu <1541131845@qq.com>
2020-03-30 00:02:20 +0800

    leetcode_5

---
+++ leetcode_5/main.c
@@ -0,0 +1,24 @@
+char * longestPalindrome(char * s){
+    if(strlen(s)==0||strlen(s)==1) return s;
+    int i,start,left,right,count,len;
+    start = len =0;
+    for(i=0;s[i]!='\0';i+=count){
+        count = 1;
+        left=i-1;
+        right = i+1;
+        while(s[right]!='\0'&&s[i]==s[right]){ //处理重复字符串
+            right++;
+            count++;
+        }
+        while(left>=0 && s[right]!='\0' && s[left] == s[right]){
+            left--;
+            right++;
+        }
+        if(right-left-1>len){
+            start = left+1;
+            len = right-left-1;
+        }
+    }
+    s[start + len] = '\0'; //原地修改返回
+    return s + start;
+}
