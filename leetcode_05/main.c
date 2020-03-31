253a04c1bf1a919ca521d72fe590c1b6f21a43e9
duchunxiu <1541131845@qq.com>
2020-04-01 00:26:25 +0800

    remove

--- leetcode_05/main.c
+++ leetcode_05/main.c
@@ -1,24 +1,33 @@
 char * longestPalindrome(char * s){
-    if(strlen(s)==0||strlen(s)==1) return s;
+    //中心扩散法
+    if(strlen(s)==0||strlen(s)==1)
+        return s;
     int i,start,left,right,count,len;
     start = len =0;
+
     for(i=0;s[i]!='\0';i+=count){
         count = 1;
         left=i-1;
         right = i+1;
+        //从右边查找重复的字符串，遇到和i相等的移动+1，直到遇到不相同的停止
+        //从左边查找重复的字符串，遇到和i相等的移动-1，直到遇到不相同的停止
         while(s[right]!='\0'&&s[i]==s[right]){ //处理重复字符串
             right++;
             count++;
-        }
+        } 
+    
+        //左右扩散，如果左边和右边相等，right++，left--
         while(left>=0 && s[right]!='\0' && s[left] == s[right]){
             left--;
             right++;
         }
+
         if(right-left-1>len){
             start = left+1;
-            len = right-left-1;
+            len = right-left-1; //更新最长回文串的长度
         }
     }
-    s[start + len] = '\0'; //原地修改返回
+
+    s[start + len] = '\0'; // 原地修改返回
     return s + start;
 }
