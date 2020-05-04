78f68cb028c829e20eff64b8bfaddc6f5c51d3b1
duchunxiu <1541131845@qq.com>
2020-05-04 21:44:18 +0800

    leetcode_028

---
+++ leetcode_028/README.md
@@ -0,0 +1,2 @@
+解题思路：
+使用memcmp即可，注意边界条件：当haystack或needle为空时return 0
---
+++ leetcode_028/main.c
@@ -0,0 +1,18 @@
+int strStr(char * haystack, char * needle){
+    int i;
+    int n_l = strlen(needle);
+    int h_l = strlen(haystack);
+    
+    if(NULL == haystack || NULL == needle || 0 == n_l)
+        return 0;
+
+    for(i = 0; (i + n_l) <= h_l ; i++)
+    {
+        if(0 == memcmp(&haystack[i], needle, n_l))
+        {
+            return i;
+        }
+    }
+
+    return -1;
+}
